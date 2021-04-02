8c5ffcfd39ec146ae75b9fd6a2edee1339eddbe4
unknown <suyashgaikwad31@gmail.com>
2021-04-02 21:50:49 +0530

    Addeed

--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -3,7 +3,8 @@ package censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType {
-        CENSUS_FILE_PROBLEM
+        CENSUS_FILE_PROBLEM, UNABLE_TO_PARSE;
+
     }
 
     ExceptionType type;
